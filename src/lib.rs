a04202c925d9089d8a4845791df934d185a9c82d
adamcanray <adamcool180@gmail.com>
2023-04-11 16:39:29 +0700

    change range end with lowercase "z" to include the lowercase and check with is_ascii_alphabetic to exclude special chars etc

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,14 @@ use std::ops::Range;
 pub fn generate_random_code(length: usize) -> String {
     let mut rng = thread_rng();
     let code: String = (0..length)
-        .map(|_| rng.gen_range(Range{start: b'A', end: b'Z'+1}) as char)
+        .map(|_| {
+            loop {
+                let c = rng.gen_range(Range{start: b'A', end: b'z'+1}) as char;
+                if c.is_ascii_alphabetic() {
+                    break c;
+                }
+            }
+        })
         .collect();
     code
 }
